4c5ab15e25f9518ecc6a5aa5e886e33c037007db
paolomococci <paolomococci@gmail.com>
2021-10-31 14:08:51 +0100

    I have completed package entity of project sample-planning

--- sample-planning/src/main/java/local/mocaccino/planning/entity/Billboard.java
+++ sample-planning/src/main/java/local/mocaccino/planning/entity/Billboard.java
@@ -1,4 +1,68 @@
 package local.mocaccino.planning.entity;
 
+import org.optaplanner.core.api.domain.solution.PlanningEntityCollectionProperty;
+import org.optaplanner.core.api.domain.solution.PlanningScore;
+import org.optaplanner.core.api.domain.solution.PlanningSolution;
+import org.optaplanner.core.api.domain.solution.ProblemFactCollectionProperty;
+import org.optaplanner.core.api.domain.valuerange.ValueRangeProvider;
+import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
+import org.optaplanner.core.api.solver.SolverStatus;
+
+import java.util.List;
+
+@PlanningSolution
 public class Billboard {
+
+    @ProblemFactCollectionProperty
+    @ValueRangeProvider(id = "auditoriumRange")
+    private List<Auditorium> auditoriums;
+
+    @ProblemFactCollectionProperty
+    @ValueRangeProvider(id = "timeslotRange")
+    private List<Timeslot> timeslots;
+
+    @PlanningEntityCollectionProperty
+    private List<Lecture> lectures;
+
+    @PlanningScore
+    private HardSoftScore score;
+
+    private SolverStatus solverStatus;
+
+    public Billboard() {
+    }
+
+    public Billboard(
+            List<Auditorium> auditoriums,
+            List<Timeslot> timeslots,
+            List<Lecture> lectures
+    ) {
+        this.auditoriums = auditoriums;
+        this.timeslots = timeslots;
+        this.lectures = lectures;
+    }
+
+    public List<Auditorium> getAuditoriums() {
+        return auditoriums;
+    }
+
+    public List<Timeslot> getTimeslots() {
+        return timeslots;
+    }
+
+    public List<Lecture> getLectures() {
+        return lectures;
+    }
+
+    public HardSoftScore getScore() {
+        return score;
+    }
+
+    public SolverStatus getSolverStatus() {
+        return solverStatus;
+    }
+
+    public void setSolverStatus(SolverStatus solverStatus) {
+        this.solverStatus = solverStatus;
+    }
 }
--- sample-planning/src/main/java/local/mocaccino/planning/entity/Timeslot.java
+++ sample-planning/src/main/java/local/mocaccino/planning/entity/Timeslot.java
@@ -1,4 +1,99 @@
 package local.mocaccino.planning.entity;
 
+import org.optaplanner.core.api.domain.lookup.PlanningId;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+@Entity
 public class Timeslot {
+
+    @PlanningId
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private DayOfWeek day;
+    private LocalTime start;
+    private LocalTime end;
+
+    public Timeslot() {
+    }
+
+    public Timeslot(
+            DayOfWeek day,
+            LocalTime start,
+            LocalTime end
+    ) {
+        this.day = day;
+        this.start = start;
+        this.end = end;
+    }
+
+    public Timeslot(
+            Long id,
+            DayOfWeek day,
+            LocalTime start
+    ) {
+        this.id = id;
+        this.day = day;
+        this.start = start;
+    }
+
+    public Timeslot(
+            Long id,
+            DayOfWeek day,
+            LocalTime start,
+            LocalTime end
+    ) {
+        this.id = id;
+        this.day = day;
+        this.start = start;
+        this.end = end;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public DayOfWeek getDay() {
+        return day;
+    }
+
+    public void setDay(DayOfWeek day) {
+        this.day = day;
+    }
+
+    public LocalTime getStart() {
+        return start;
+    }
+
+    public void setStart(LocalTime start) {
+        this.start = start;
+    }
+
+    public LocalTime getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalTime end) {
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return "Timeslot{" +
+                "id=" + id +
+                ", day=" + day +
+                ", start=" + start +
+                ", end=" + end +
+                '}';
+    }
 }
