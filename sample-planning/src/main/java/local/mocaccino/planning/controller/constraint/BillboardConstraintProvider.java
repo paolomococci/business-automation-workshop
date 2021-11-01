ad407d8c289a91c5a7ca79bca2e6e3f07f84738a
paolomococci <37364757+paolomococci@users.noreply.github.com>
2021-11-01 05:59:55 +0100

    Update BillboardConstraintProvider.java

--- sample-planning/src/main/java/local/mocaccino/planning/controller/constraint/BillboardConstraintProvider.java
+++ sample-planning/src/main/java/local/mocaccino/planning/controller/constraint/BillboardConstraintProvider.java
@@ -1,4 +1,14 @@
 package local.mocaccino.planning.controller.constraint;
 
-public class BillboardConstraintProvider {
+import org.optaplanner.core.api.score.stream.Constraint;
+import org.optaplanner.core.api.score.stream.ConstraintFactory;
+import org.optaplanner.core.api.score.stream.ConstraintProvider;
+
+public class BillboardConstraintProvider
+        implements ConstraintProvider {
+
+    @Override
+    public Constraint[] defineConstraints(ConstraintFactory constraintFactory) {
+        return new Constraint[0];
+    }
 }
