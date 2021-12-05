d41e583bc1f311411a71949fc64b05c5816269b9
paolomococci <paolomococci@gmail.com>
2021-12-05 19:25:47 +0100

    added AntiHiccupTest class to project anti-hiccup

---
+++ anti-hiccup/src/test/java/local/mocaccino/planning/AntiHiccupTest.java
@@ -0,0 +1,20 @@
+package local.mocaccino.planning;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class AntiHiccupTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/hello")
+          .then()
+             .statusCode(200)
+             .body(is("Good morning efficiency!"));
+    }
+}
