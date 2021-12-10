96ef9d785cf464dd3ec33a16c4cee4a6b021f0f9
paolomococci <paolomococci@gmail.com>
2021-12-10 07:30:16 +0100

    added Application class to welcome project

---
+++ welcome/src/main/java/local/example/welcome/Application.java
@@ -0,0 +1,22 @@
+package local.example.welcome;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+@Theme(value = "welcome", variant = Lumo.DARK)
+@PWA(name = "Welcome", shortName = "Welcome", offlineResources = { "images/logo.png" })
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
