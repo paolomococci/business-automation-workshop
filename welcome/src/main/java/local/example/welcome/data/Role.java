4e8f71bdb0abe78ae65bdaa871fd6e163b544c62
paolomococci <paolomococci@gmail.com>
2021-12-10 07:32:24 +0100

    added Role class to welcome project

---
+++ welcome/src/main/java/local/example/welcome/data/Role.java
@@ -0,0 +1,15 @@
+package local.example.welcome.data;
+
+public enum Role {
+    USER("user"), ADMIN("admin");
+
+    private String roleName;
+
+    private Role(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+}
