55fdad089ea00005c82030f0b14f802b4b26e5cc
paolomococci <paolomococci@gmail.com>
2021-12-15 15:31:58 +0100

    edited the class Address belonging to project twentytwo

--- twentytwo/src/main/java/local/example/demo/data/entity/Address.java
+++ twentytwo/src/main/java/local/example/demo/data/entity/Address.java
@@ -1,6 +1,11 @@
 package local.example.demo.data.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import local.example.demo.data.AbstractEntity;
 
 @Entity
@@ -12,6 +17,16 @@ public class Address extends AbstractEntity {
     private String state;
     private String country;
 
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_customer_fk")
+    private Customer customer;
+
+    public Customer getCustomer() {
+        return customer;
+    }
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
     public String getStreet() {
         return street;
     }
