fdccfacaa3fb6ac382b005d5c867c6d0c79188c5
paolomococci <paolomococci@gmail.com>
2021-12-15 15:32:32 +0100

    edited the class Customer belonging to project twentytwo

--- twentytwo/src/main/java/local/example/demo/data/entity/Customer.java
+++ twentytwo/src/main/java/local/example/demo/data/entity/Customer.java
@@ -1,7 +1,14 @@
 package local.example.demo.data.entity;
 
 import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
 import local.example.demo.data.AbstractEntity;
 
 @Entity
@@ -13,6 +20,25 @@ public class Customer extends AbstractEntity {
     private String phone;
     private LocalDate birthday;
     private String occupation;
+
+    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
+    private List<Address> addresses = new LinkedList<>();
+
+    @ManyToMany(mappedBy = "bookCustomer")
+    private List<Book> books = new LinkedList<>();
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
+    public List<Book> getBooks() {
+        return books;
+    }
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
 
     public String getName() {
         return name;
