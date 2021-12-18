e2ee6e53b5ddbc686adbf8a84a3e8e3cc9b91cb6
paolomococci <paolomococci@gmail.com>
2021-12-18 09:01:39 +0100

    edited the class BookField belonging to project twentytwo again

--- twentytwo/src/main/java/local/example/demo/views/customer/field/BookField.java
+++ twentytwo/src/main/java/local/example/demo/views/customer/field/BookField.java
@@ -3,6 +3,7 @@ package local.example.demo.views.customer.field;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.customfield.CustomField;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.data.renderer.TextRenderer;
 import local.example.demo.data.entity.Book;
 import local.example.demo.data.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,7 @@ public class BookField extends CustomField<Book> {
     public BookField(String label) {
         setLabel(label);
         this.book.setPlaceholder("Book");
+        this.book.setRenderer(new TextRenderer<>(entity -> entity.getTitle()));
         this.book.setItems(this.bookService.list());
         HorizontalLayout horizontalLayout = new HorizontalLayout(this.book);
         horizontalLayout.setFlexGrow(1.0, this.book);
