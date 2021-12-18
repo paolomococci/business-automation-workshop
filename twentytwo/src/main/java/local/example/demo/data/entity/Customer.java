9722caffe9ac9c9b65a64ce580b9671038b63107
paolomococci <paolomococci@gmail.com>
2021-12-18 20:20:07 +0100

    added the class Customer belonging to project twentytwo

--- twentytwo/src/main/java/local/example/demo/data/entity/Customer.java
+++ twentytwo/src/main/java/local/example/demo/data/entity/Customer.java
@@ -30,12 +30,15 @@ public class Customer extends AbstractEntity {
     public List<Address> getAddresses() {
         return addresses;
     }
+
     public void setAddresses(List<Address> addresses) {
         this.addresses = addresses;
     }
+
     public List<Book> getBooks() {
         return books;
     }
+
     public void setBooks(List<Book> books) {
         this.books = books;
     }
@@ -43,36 +46,47 @@ public class Customer extends AbstractEntity {
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getSurname() {
         return surname;
     }
+
     public void setSurname(String surname) {
         this.surname = surname;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getPhone() {
         return phone;
     }
+
     public void setPhone(String phone) {
         this.phone = phone;
     }
+
     public LocalDate getBirthday() {
         return birthday;
     }
+
     public void setBirthday(LocalDate birthday) {
         this.birthday = birthday;
     }
+
     public String getOccupation() {
         return occupation;
     }
+
     public void setOccupation(String occupation) {
         this.occupation = occupation;
     }
