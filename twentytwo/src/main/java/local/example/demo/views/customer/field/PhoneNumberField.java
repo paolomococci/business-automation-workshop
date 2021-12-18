63ad0bbfacca777a82928eebc5d160abe52a51c5
paolomococci <paolomococci@gmail.com>
2021-12-18 19:25:49 +0100

    added the class PhoneNumberField belonging to project twentytwo

--- twentytwo/src/main/java/local/example/demo/views/customer/field/PhoneNumberField.java
+++ twentytwo/src/main/java/local/example/demo/views/customer/field/PhoneNumberField.java
@@ -11,24 +11,28 @@ public class PhoneNumberField extends CustomField<String> {
     private final TextField number = new TextField();
 
     public PhoneNumberField(String label) {
-        setLabel(label);
-        countryCode.setWidth("120px");
-        countryCode.setPlaceholder("Country");
-        countryCode.setPattern("\\+\\d*");
-        countryCode.setPreventInvalidInput(true);
-        countryCode.setItems("+354", "+91", "+62", "+98", "+964", "+353", "+44", "+972", "+39", "+225");
-        countryCode.addCustomValueSetListener(e -> countryCode.setValue(e.getDetail()));
-        number.setPattern("\\d*");
-        number.setPreventInvalidInput(true);
-        HorizontalLayout horizontalLayout = new HorizontalLayout(countryCode, number);
-        horizontalLayout.setFlexGrow(1.0, number);
-        add(horizontalLayout);
+        this.setLabel(label);
+        this.countryCode.setWidth("120px");
+        this.countryCode.setPlaceholder("Country");
+        this.countryCode.setPattern("\\+\\d*");
+        this.countryCode.setPreventInvalidInput(true);
+        this.countryCode.setItems("+354", "+91", "+62", "+98", "+964", "+353", "+44", "+972", "+39", "+225");
+        this.countryCode.addCustomValueSetListener(e -> this.countryCode.setValue(e.getDetail()));
+        this.number.setPattern("\\d*");
+        this.number.setPreventInvalidInput(true);
+        HorizontalLayout horizontalLayout = new HorizontalLayout(
+                this.countryCode,
+                this.number
+        );
+        horizontalLayout.setFlexGrow(1.0, this.number);
+
+        this.add(horizontalLayout);
     }
 
     @Override
     protected String generateModelValue() {
-        if (countryCode.getValue() != null && number.getValue() != null) {
-            return countryCode.getValue() + " " + number.getValue();
+        if (this.countryCode.getValue() != null && this.number.getValue() != null) {
+            return this.countryCode.getValue() + " " + this.number.getValue();
         }
         return "";
     }
@@ -37,14 +41,14 @@ public class PhoneNumberField extends CustomField<String> {
     protected void setPresentationValue(String phoneNumber) {
         String[] parts = phoneNumber != null ? phoneNumber.split(" ", 2) : new String[0];
         if (parts.length == 1) {
-            countryCode.clear();
-            number.setValue(parts[0]);
+            this.countryCode.clear();
+            this.number.setValue(parts[0]);
         } else if (parts.length == 2) {
-            countryCode.setValue(parts[0]);
-            number.setValue(parts[1]);
+            this.countryCode.setValue(parts[0]);
+            this.number.setValue(parts[1]);
         } else {
-            countryCode.clear();
-            number.clear();
+            this.countryCode.clear();
+            this.number.clear();
         }
     }
 }
