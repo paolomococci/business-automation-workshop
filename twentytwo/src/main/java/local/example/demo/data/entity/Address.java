1a21a74937179cd2ec70bf55c0fe9297df270a6f
paolomococci <paolomococci@gmail.com>
2021-12-18 20:21:18 +0100

    modified the class Address belonging to project twentytwo

--- twentytwo/src/main/java/local/example/demo/data/entity/Address.java
+++ twentytwo/src/main/java/local/example/demo/data/entity/Address.java
@@ -24,36 +24,47 @@ public class Address extends AbstractEntity {
     public Customer getCustomer() {
         return customer;
     }
+
     public void setCustomer(Customer customer) {
         this.customer = customer;
     }
+
     public String getStreet() {
         return street;
     }
+
     public void setStreet(String street) {
         this.street = street;
     }
+
     public String getPostalCode() {
         return postalCode;
     }
+
     public void setPostalCode(String postalCode) {
         this.postalCode = postalCode;
     }
+
     public String getCity() {
         return city;
     }
+
     public void setCity(String city) {
         this.city = city;
     }
+
     public String getState() {
         return state;
     }
+
     public void setState(String state) {
         this.state = state;
     }
+
     public String getCountry() {
         return country;
     }
+
     public void setCountry(String country) {
         this.country = country;
     }
