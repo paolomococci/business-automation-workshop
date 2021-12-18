1d9716d2a252ca1fd87b838c14f1afc543f61332
paolomococci <paolomococci@gmail.com>
2021-12-18 09:26:05 +0100

    edited the class AddressField belonging to project twentytwo again

--- twentytwo/src/main/java/local/example/demo/views/customer/field/AddressField.java
+++ twentytwo/src/main/java/local/example/demo/views/customer/field/AddressField.java
@@ -3,6 +3,7 @@ package local.example.demo.views.customer.field;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.customfield.CustomField;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.data.renderer.TextRenderer;
 import local.example.demo.data.entity.Address;
 import local.example.demo.data.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,12 +13,13 @@ public class AddressField extends CustomField<Address> {
     @Autowired
     AddressService addressService;
 
-    private final ComboBox<String> address = new ComboBox<>();
+    private final ComboBox<Address> address = new ComboBox<>();
 
     public AddressField(String label) {
         setLabel(label);
         this.address.setPlaceholder("Address");
-        this.address.setItems();
+        this.address.setRenderer(new TextRenderer<>(entity -> entity.toString()));
+        this.address.setItems(this.addressService.list());
         HorizontalLayout horizontalLayout = new HorizontalLayout(this.address);
         horizontalLayout.setFlexGrow(1.0, this.address);
         add(horizontalLayout);
