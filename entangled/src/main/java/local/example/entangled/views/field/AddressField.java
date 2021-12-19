a612fb22ca3e1f63bb3db7a70303be891fd4486e
paolomococci <paolomococci@gmail.com>
2021-12-19 17:42:48 +0100

    edited class AddressField belonging to project entangled

--- entangled/src/main/java/local/example/entangled/views/field/AddressField.java
+++ entangled/src/main/java/local/example/entangled/views/field/AddressField.java
@@ -3,14 +3,23 @@ package local.example.entangled.views.field;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.customfield.CustomField;
 import local.example.entangled.data.entity.Address;
+import local.example.entangled.data.service.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class AddressField
         extends CustomField<Address> {
 
+    private final AddressService addressService;
     private final ComboBox<Address> addressComboBox;
 
-    public AddressField(ComboBox<Address> addressComboBox) {
-        this.addressComboBox = addressComboBox;
+    public AddressField(
+            @Autowired AddressService addressService
+    ) {
+        this.addressService = addressService;
+        this.addressComboBox = new ComboBox<>("Address");
+        this.addressComboBox.setItems(this.addressService.list());
+        this.addressComboBox.setItemLabelGenerator(Address::getStreet);
+        this.add(this.addressComboBox);
     }
 
     @Override
