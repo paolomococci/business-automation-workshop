639abbc9021e69da2ff976fcb90892fbd807fd12
paolomococci <paolomococci@gmail.com>
2021-12-19 16:53:13 +0100

    edited class GuestField belonging to project entangled

--- entangled/src/main/java/local/example/entangled/views/field/GuestField.java
+++ entangled/src/main/java/local/example/entangled/views/field/GuestField.java
@@ -1,4 +1,18 @@
 package local.example.entangled.views.field;
 
-public class GuestField {
+import com.vaadin.flow.component.customfield.CustomField;
+import local.example.entangled.data.entity.Guest;
+
+public class GuestField
+        extends CustomField<Guest> {
+
+    @Override
+    protected Guest generateModelValue() {
+        return null;
+    }
+
+    @Override
+    protected void setPresentationValue(Guest guest) {
+
+    }
 }
