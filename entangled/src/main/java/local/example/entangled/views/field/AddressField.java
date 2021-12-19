f327fedf21d42ab899fcdab3226446a20233a0f7
paolomococci <paolomococci@gmail.com>
2021-12-19 17:10:47 +0100

    edited class AddressField belonging to project entangled

--- entangled/src/main/java/local/example/entangled/views/field/AddressField.java
+++ entangled/src/main/java/local/example/entangled/views/field/AddressField.java
@@ -1,10 +1,17 @@
 package local.example.entangled.views.field;
 
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.customfield.CustomField;
 import local.example.entangled.data.entity.Address;
 
 public class AddressField
         extends CustomField<Address> {
+
+    private ComboBox<Address> addressComboBox;
+
+    public AddressField(ComboBox<Address> addressComboBox) {
+        this.addressComboBox = addressComboBox;
+    }
 
     @Override
     protected Address generateModelValue() {
