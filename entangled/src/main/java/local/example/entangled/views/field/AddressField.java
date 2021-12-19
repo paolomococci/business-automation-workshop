0542542a47f567c5209727a64d1067d7c3e4c24e
paolomococci <paolomococci@gmail.com>
2021-12-19 16:52:55 +0100

    edited class AddressField belonging to project entangled

--- entangled/src/main/java/local/example/entangled/views/field/AddressField.java
+++ entangled/src/main/java/local/example/entangled/views/field/AddressField.java
@@ -1,4 +1,18 @@
 package local.example.entangled.views.field;
 
-public class AddressField {
+import com.vaadin.flow.component.customfield.CustomField;
+import local.example.entangled.data.entity.Address;
+
+public class AddressField
+        extends CustomField<Address> {
+
+    @Override
+    protected Address generateModelValue() {
+        return null;
+    }
+
+    @Override
+    protected void setPresentationValue(Address address) {
+
+    }
 }
