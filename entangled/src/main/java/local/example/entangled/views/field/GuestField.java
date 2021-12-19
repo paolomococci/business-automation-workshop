f12a26cfb3eb1ffd04495175ddf0b148e593e955
paolomococci <paolomococci@gmail.com>
2021-12-19 17:10:29 +0100

    edited class GuestField belonging to project entangled

--- entangled/src/main/java/local/example/entangled/views/field/GuestField.java
+++ entangled/src/main/java/local/example/entangled/views/field/GuestField.java
@@ -1,10 +1,17 @@
 package local.example.entangled.views.field;
 
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.customfield.CustomField;
 import local.example.entangled.data.entity.Guest;
 
 public class GuestField
         extends CustomField<Guest> {
+
+    private ComboBox<Guest> guestComboBox;
+
+    public GuestField(ComboBox<Guest> guestComboBox) {
+        this.guestComboBox = guestComboBox;
+    }
 
     @Override
     protected Guest generateModelValue() {
