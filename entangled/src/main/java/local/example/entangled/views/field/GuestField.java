26483769266f058e266be4427fa7bb987e28ae1d
paolomococci <paolomococci@gmail.com>
2021-12-19 17:42:33 +0100

    edited class GuestField belonging to project entangled

--- entangled/src/main/java/local/example/entangled/views/field/GuestField.java
+++ entangled/src/main/java/local/example/entangled/views/field/GuestField.java
@@ -3,14 +3,23 @@ package local.example.entangled.views.field;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.customfield.CustomField;
 import local.example.entangled.data.entity.Guest;
+import local.example.entangled.data.service.GuestService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class GuestField
         extends CustomField<Guest> {
 
+    private final GuestService guestService;
     private final ComboBox<Guest> guestComboBox;
 
-    public GuestField(ComboBox<Guest> guestComboBox) {
-        this.guestComboBox = guestComboBox;
+    public GuestField(
+            @Autowired GuestService guestService
+    ) {
+        this.guestService = guestService;
+        this.guestComboBox = new ComboBox<>("Guest");
+        this.guestComboBox.setItems(this.guestService.list());
+        this.guestComboBox.setItemLabelGenerator(Guest::getUsername);
+        this.add(this.guestComboBox);
     }
 
     @Override
