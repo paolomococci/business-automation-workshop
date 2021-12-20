cf79c497094f5b039511c7498d55fbc761bacc94
paolomococci <paolomococci@gmail.com>
2021-12-20 10:23:48 +0100

    modified the class CustomerService belonging to project twentytwo

--- twentytwo/src/main/java/local/example/demo/data/service/CustomerService.java
+++ twentytwo/src/main/java/local/example/demo/data/service/CustomerService.java
@@ -1,7 +1,5 @@
 package local.example.demo.data.service;
 
-import java.util.List;
-import java.util.Optional;
 import local.example.demo.data.entity.Customer;
 import local.example.demo.data.repository.CustomerRepository;
 
@@ -9,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
