dbd1d43e1c9016547c91a9a05f786caa49ca4654
paolomococci <paolomococci@gmail.com>
2022-09-12 09:58:59 +0200

    modified:   Spring-Boot/entangled/src/main/java/local/example/entangled/views/home/HomeView.java

--- Spring-Boot/entangled/src/main/java/local/example/entangled/views/home/HomeView.java
+++ Spring-Boot/entangled/src/main/java/local/example/entangled/views/home/HomeView.java
@@ -1,8 +1,6 @@
 package local.example.entangled.views.home;
 
 import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
