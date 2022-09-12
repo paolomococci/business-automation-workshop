8476fda0da90c1582098e418307a4e04ff1989de
paolomococci <paolomococci@gmail.com>
2022-09-12 08:50:18 +0200

    new file:   Spring-Boot/twenty-three/src/main/java/local/example/demo/data/entity/Customer.java

---
+++ Spring-Boot/twenty-three/src/main/java/local/example/demo/data/entity/Customer.java
@@ -0,0 +1,90 @@
+package local.example.demo.data.entity;
+
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.persistence.*;
+
+import local.example.demo.data.AbstractEntity;
+
+@Entity
+public class Customer extends AbstractEntity {
+
+    private String name;
+    private String surname;
+    private String email;
+    private String phone;
+    private LocalDate birthday;
+    private String occupation;
+
+    @OneToMany(mappedBy = "customer", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<Address> addresses = new LinkedList<>();
+
+    @ManyToMany(mappedBy = "bookCustomer")
+    private List<Book> books = new LinkedList<>();
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+}
